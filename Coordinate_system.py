9a287973319010708c3e9026b565184cd41af0ae
Stokes <jim159093@gmail.com>
2016-07-10 01:50:21 +0800

    修改內容
    原本是把座標的內容家在object底下，但使想說這樣會不會在物件過多的情況下，增加了太多Property，所以改為在scene底下的一個Prop
    ertyGroup
    
    ———
    問題：
    如何讓這個在scene底下Sphere_coordinate的值，可以在active_object之Position修改時跟著連動被修改？

--- Coordinate_system.py
+++ Coordinate_system.py
@@ -2,21 +2,21 @@ import bpy
 import mathutils
 import math
 from bpy.props import *
-       
-ob_location = bpy.context.object.location
-ob_t1 = math.degrees(math.atan(math.sqrt(math.pow(ob_location.x,2)+math.pow(ob_location.y,2))/ob_location.z))
-ob_t2 = math.degrees(math.atan2(ob_location.y,ob_location.x))
-    
+
+#更動求座標系時呼叫來更動active_object座標系之函數       
 def sphere_update(self, context):
-    r = context.object.mysphere_radius
-    t1 = math.radians(context.object.mysphere_t1)
-    t2 = math.radians(context.object.mysphere_t2)
+    sphere_coordinate = context.scene.sphere_coordinate
+    r = sphere_coordinate.radius
+    t1 = math.radians(sphere_coordinate.polar)
+    t2 = math.radians(sphere_coordinate.azimuth)
     x = r*math.sin(t1)*math.cos(t2)
     y = r*math.sin(t1)*math.sin(t2)
     z = r*math.cos(t1)
     context.object.location.x = x
     context.object.location.y = y
     context.object.location.z = z
+"""
+期望在Position更動時跟著呼叫的函數
 
 def test1(self, context):
     r = bpy.context.object.location.length
@@ -25,51 +25,55 @@ def test1(self, context):
     bpy.types.Object.mysphere_radius = r
     bpy.types.Object.mysphere_t1 = t1
     bpy.types.Object.mysphere_t2 = t2
+"""
 
-bpy.types.Object.mysphere_radius = FloatProperty(
-    name = "r", 
-    default = bpy.context.object.location.length,
-    update = sphere_update)
+#將放在scene下的一個PropertyGroup，用來儲存半徑、天頂角、方位角的數值
+class Sphere_coordinate(bpy.types.PropertyGroup):
+    radius = FloatProperty(
+        name = "r", 
+        default = 0,
+        update = sphere_update)
 
-bpy.types.Object.mysphere_t1 = FloatProperty(
-    name = "θ", 
-    default = ob_t1, min = 0, max = 360,
-    update = sphere_update)
+    polar = FloatProperty(
+        name = "θ", 
+        default = 0, min = 0, max = 360,
+        update = sphere_update)
 
-bpy.types.Object.mysphere_t2 = FloatProperty(
-    name = "φ", 
-    default = ob_t2, min = 0, max = 360,
-    update = sphere_update)
-    
-class LayoutDemoPanel(bpy.types.Panel):
+    azimuth = FloatProperty(
+        name = "φ", 
+        default = 0, min = 0, max = 360,
+        update = sphere_update)    
+
+#在Scene底下加入Sphere_coordinate的PropertyGroup             
+bpy.utils.register_class(Sphere_coordinate)
+bpy.types.Scene.sphere_coordinate = bpy.props.PointerProperty(type = Sphere_coordinate)
+
+
+#UI部分
+class Coodinate_system(bpy.types.Panel):
     """Creates a Panel in the scene context of the properties editor"""
-    bl_label = "myTransform"
-    #bl_idname = "myTransform"
+    bl_label = "Coordinate System"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
-    #bl_context = "scene"
     
 
     def draw(self, context):
         layout = self.layout
 
-        scene = context.scene
-
-        # Create a simple row.
-        active_obj = bpy.context.active_object
+        Sphere_coordinate = context.scene.sphere_coordinate
         
         layout.label(text = "sphere coordinate")
         row = layout.row()
-        row.column().prop(active_obj, "mysphere_radius")
-        layout.column().prop(active_obj, "mysphere_t1")
-        layout.column().prop(active_obj, "mysphere_t2")
+        row.column().prop(Sphere_coordinate, "radius")
+        layout.column().prop(Sphere_coordinate, "polar")
+        layout.column().prop(Sphere_coordinate, "azimuth")
 
 def register():
-    bpy.utils.register_class(LayoutDemoPanel)
+    bpy.utils.register_class(Coodinate_system)
 
 
 def unregister():
-    bpy.utils.unregister_class(LayoutDemoPanel)
+    bpy.utils.unregister_class(Coodinate_system)
 
 
 if __name__ == "__main__":
