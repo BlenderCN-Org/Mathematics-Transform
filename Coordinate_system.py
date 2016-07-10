44787cde743937278c4006129a8b761a454a56c0
Stokes <jim159093@gmail.com>
2016-07-10 23:00:10 +0800

    修改變數名稱及定義域
    一些變數的名稱看起來不夠可讀，故修改，將t1,t2修改為polar,azimuth，並把函數的名稱由sphere_update修改update_
    Cartesian，意思為修改直角座標系的值，而r,θ,φ三者的定義域先回歸到數學上較嚴格的規定。

--- Coordinate_system.py
+++ Coordinate_system.py
@@ -4,53 +4,56 @@ import math
 from bpy.props import *
 
 #更動求座標系時呼叫來更動active_object座標系之函數       
-def sphere_update(self, context):
-    sphere_coordinate = context.scene.sphere_coordinate
-    r = sphere_coordinate.radius
-    t1 = math.radians(sphere_coordinate.polar)
-    t2 = math.radians(sphere_coordinate.azimuth)
-    x = r*math.sin(t1)*math.cos(t2)
-    y = r*math.sin(t1)*math.sin(t2)
-    z = r*math.cos(t1)
+def update_Cartesian(self, context):
+    Sphere_Coordinate = context.scene.Sphere_Coordinate
+    r = Sphere_Coordinate.radius
+    polar = math.radians(Sphere_Coordinate.polar)
+    azimuth = math.radians(Sphere_Coordinate.azimuth)
+    x = r*math.sin(polar)*math.cos(azimuth)
+    y = r*math.sin(polar)*math.sin(azimuth)
+    z = r*math.cos(polar)
     context.object.location.x = x
     context.object.location.y = y
     context.object.location.z = z
-"""
-期望在Position更動時跟著呼叫的函數
+    
+#預期用來修改球座標系的函數
+def update_Sphere(self, context):
+    ob = bpy.context.scene.Cartesian_coordinate
+    radius = ob.length
+    polar= math.degrees(math.acos(ob.z/radius))
+    azimuth = math.degrees(math.atan2(ob.y,ob.x))
+    Sphere_Coordinate = context.scene.Sphere_Coordinate
+    Sphere_Coordinate.radius = radius
+    Sphere_Coordinate.polar = polar
+    Sphere_Coordinate.azimuth = azimuth
 
-def test1(self, context):
-    r = bpy.context.object.location.length
-    t1= math.degrees(math.atan(math.sqrt(math.pow(ob_location.x,2)+math.pow(ob_location.y,2))/ob_location.z))
-    t2 = math.degrees(math.atan2(ob_location.y,ob_location.x))
-    bpy.types.Object.mysphere_radius = r
-    bpy.types.Object.mysphere_t1 = t1
-    bpy.types.Object.mysphere_t2 = t2
-"""
 
+#Cartesian coordinate
 #將放在scene下的一個PropertyGroup，用來儲存半徑、天頂角、方位角的數值
-class Sphere_coordinate(bpy.types.PropertyGroup):
+class Sphere_Coordinate(bpy.types.PropertyGroup):
     radius = FloatProperty(
         name = "r", 
-        default = 0,
-        update = sphere_update)
+        default = 0, min = 0,
+        update = update_Cartesian)
 
     polar = FloatProperty(
         name = "θ", 
-        default = 0, min = 0, max = 360,
-        update = sphere_update)
+        default = 0, min = 0, max = 180,
+        update = update_Cartesian)
 
     azimuth = FloatProperty(
         name = "φ", 
-        default = 0, min = 0, max = 360,
-        update = sphere_update)    
-
-#在Scene底下加入Sphere_coordinate的PropertyGroup             
-bpy.utils.register_class(Sphere_coordinate)
-bpy.types.Scene.sphere_coordinate = bpy.props.PointerProperty(type = Sphere_coordinate)
-
+        default = 0, min = -180, max = 180,
+        update = update_Cartesian)
+    
+#在Scene底下加入Sphere_coordinate的PropertyGroup         
+       
+bpy.utils.register_class(Sphere_Coordinate)
+bpy.types.Scene.Sphere_Coordinate = bpy.props.PointerProperty(type = Sphere_Coordinate)
+bpy.types.Scene.Cartesian_Coordinate = FloatVectorProperty(subtype = 'TRANSLATION',update = update_Sphere)
 
 #UI部分
-class Coodinate_system(bpy.types.Panel):
+class Coodinate_system_Panel(bpy.types.Panel):
     """Creates a Panel in the scene context of the properties editor"""
     bl_label = "Coordinate System"
     bl_space_type = 'VIEW_3D'
@@ -59,22 +62,22 @@ class Coodinate_system(bpy.types.Panel):
 
     def draw(self, context):
         layout = self.layout
-
-        Sphere_coordinate = context.scene.sphere_coordinate
         
-        layout.label(text = "sphere coordinate")
+        scene = context.scene
+        Sphere_Coordinate = context.scene.Sphere_Coordinate
+
+        layout.label(text = "Sphere Coordinate")
         row = layout.row()
-        row.column().prop(Sphere_coordinate, "radius")
-        layout.column().prop(Sphere_coordinate, "polar")
-        layout.column().prop(Sphere_coordinate, "azimuth")
-
+        row.column().prop(Sphere_Coordinate, "radius")
+        layout.column().prop(Sphere_Coordinate, "polar")
+        layout.column().prop(Sphere_Coordinate, "azimuth")
+            
 def register():
-    bpy.utils.register_class(Coodinate_system)
-
+    
+    bpy.utils.register_class(Coodinate_system_Panel)
 
 def unregister():
-    bpy.utils.unregister_class(Coodinate_system)
-
+    bpy.utils.unregister_class(Coodinate_system_Panel)
 
 if __name__ == "__main__":
     register()
